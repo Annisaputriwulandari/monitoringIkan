2b5a5f18db0b5342f8716562a7ba5c6cf1226f03
Annisaputriwulandari <annisapwulandari@gmail.com>
2022-07-04 17:35:49 +0700

    about

---
+++ app/src/main/java/com/bagicode/smartfarm/AboutActivity.kt
@@ -0,0 +1,15 @@
+package com.bagicode.smartfarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+
+    }
+
+}
